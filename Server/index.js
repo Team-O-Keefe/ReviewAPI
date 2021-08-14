ccee5d1612391db57290f43e7018ae401c89f0e0
Corey Robinson <cr.contact90@gmail.com>
2021-08-14 10:13:07 -0700

    Updated linter

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable brace-style */
 /* eslint-disable camelcase */
 var express = require('express');
 const morgan = require('morgan');
@@ -12,7 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/reviews', (req, res) => {
   // eslint-disable-next-line no-unused-vars
-  const { product_id,
+  const {
+    product_id,
     page,
     count,
     sort
@@ -32,30 +34,25 @@ app.get('/reviews', (req, res) => {
     if (err) {
       console.log('err on review query', err);
     } else {
-      res.send({ product: product_id, count: count || 5, page: page || 1, results: result.rows });
+      res.send({
+        product: product_id, count: count || 5, page: page || 1, results: result.rows
+      });
     }
   });
-
-
-
-
-
-
 });
 
 app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`)
-})
-
+  console.log(`app listening at http://localhost:${port}`);
+});
 
 // current working
-// `SELECT reviews.id AS review_id, reviews.rating, reviews.date, reviews.summary, reviews.body, reviews.recommend, reviews.reviewer_name, reviews.reviewer_email, reviews.response, reviews.helpfulness, json_agg(json_build_object('id', reviewPhoto.id, 'url', reviewphoto.url)) AS photos
+// `SELECT reviews.id AS review_id, reviews.rating, reviews.date, reviews.summary, reviews.body,
+// reviews.recommend, reviews.reviewer_name, reviews.response, reviews.helpfulness,
+// json_agg(json_build_object('id', reviewPhoto.id, 'url', reviewphoto.url)) AS photos
 // from reviews LEFT JOIN reviewPhoto
 // ON reviewPhoto.review_id = reviews.id
 //  WHERE reviews.product_id = ${product_id} AND reviews.reported = false
 //  GROUP BY reviews.id;`;
-
-
 
 // `SELECT row_to_json(reviewList) as reviews
 // FROM(
@@ -71,12 +68,10 @@ app.listen(port, () => {
 //   ON reviews.id = reviewPhoto.review_id
 //   WHERE reviews.product_id = ${product_id};
 
-  // const query = `SELECT product_info.product_id AS id, name, slogan, description, category, default_price, jsonb_agg(json_build_object('feature', features.feature, 'value', features.value)) AS features FROM product_info JOIN features ON features.product_id = product_info.product_id WHERE product_info.product_id=${productID} GROUP BY product_info.product_id;`;
+// json_object_agg('product',${product_id} ,'results', json_agg(SELECT * from Reviews
+// WHERE Reviews.product_id = ${product_id}));`
 
-  // json_object_agg('product',${product_id} ,'results', json_agg(SELECT * from Reviews WHERE Reviews.product_id = ${product_id}));`
+// SELECT reviews.product_id, jsonb_agg(json_build_object('review_id', reviews.id)) AS results
+// FROM reviews WHERE reviews.product_id = ${product_id} GROUP BY reviews;`
 
-
-
-  // SELECT reviews.product_id, jsonb_agg(json_build_object('review_id', reviews.id)) AS results FROM reviews WHERE reviews.product_id = ${product_id} GROUP BY reviews;`
-
-  // SELECT * from reviews where product_id = ${product_id};
+// SELECT * from reviews where product_id = ${product_id};
