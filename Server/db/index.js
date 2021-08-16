8869e0283a2780ea8cbca0ec976f66f1c1620aeb
Corey Robinson <cr.contact90@gmail.com>
2021-08-16 16:14:00 -0700

    Tested pg-promise

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,14 +1,21 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 
 const pool = new Pool({
   host: "localhost",
   user: "coreyrobinson",
   port: 5432,
-  database: "Review"
-})
+  database: "Review",
+});
 
 pool.connect();
+// const pgp = require('pg-promise')();
+// const cn = {
+//   host: "localhost",
+//   user: "coreyrobinson",
+//   port: 5432,
+//   database: "Review",
+// };
 
-
+// const db = pgp(cn);
 
 module.exports = pool;
